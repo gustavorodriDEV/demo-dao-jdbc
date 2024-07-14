f82427d3893912f00266b20b35e656ee99217c22
gustavorodriDEV <gustavorodriguesoliveira012@gmail.com>
2024-07-14 02:28:24 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc  = new Scanner(System.in);
+		
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -50,7 +54,17 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Atualização completa");
+		
+		System.out.println("\n=== TEST 6: seller insert =====");
+		System.out.println("Informe um id para deletar: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+		System.out.println("Delete completo!");
 
+		
+		
+		
+		sc.close();
 	}
 
 }
